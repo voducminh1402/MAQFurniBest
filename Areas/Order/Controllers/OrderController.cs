a9d7390f6206221feafea8bed2f3a1941476d289
learning2html <honganhle0a@gmail.com>
2022-03-02 23:58:13 +0700

    test

--- Areas/Order/Controllers/OrderController.cs
+++ Areas/Order/Controllers/OrderController.cs
@@ -9,6 +9,7 @@ using MAQFurni.Models;
 
 namespace MAQFurni.Areas_Order_Controllers
 {
+    [Area("Order")]
     public class OrderController : Controller
     {
         private readonly FurnitureShopContext _context;
@@ -19,6 +20,7 @@ namespace MAQFurni.Areas_Order_Controllers
         }
 
         // GET: Order
+        [HttpGet("admin/order")]
         public async Task<IActionResult> Index()
         {
             var furnitureShopContext = _context.Orders.Include(o => o.User);
@@ -26,6 +28,7 @@ namespace MAQFurni.Areas_Order_Controllers
         }
 
         // GET: Order/Details/5
+        [HttpGet("admin/order/detail/{id}")]
         public async Task<IActionResult> Details(string id)
         {
             if (id == null)
@@ -45,17 +48,17 @@ namespace MAQFurni.Areas_Order_Controllers
         }
 
         // GET: Order/Create
+        [HttpGet("admin/order/create")]
         public IActionResult Create()
         {
-            //ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
-            ViewData["UserName"] = new SelectList(_context.Users, "UserName", "UserName");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "UserName");
             return View();
         }
 
         // POST: Order/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        [HttpPost("admin/order/create")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("OrderId,UserId,TotalPrice,CreateDate")] Order order)
         {
@@ -70,6 +73,7 @@ namespace MAQFurni.Areas_Order_Controllers
         }
 
         // GET: Order/Edit/5
+        [HttpGet("admin/order/edit/{id}")]
         public async Task<IActionResult> Edit(string id)
         {
             if (id == null)
@@ -82,14 +86,14 @@ namespace MAQFurni.Areas_Order_Controllers
             {
                 return NotFound();
             }
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", order.UserId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "UserName", order.UserId);
             return View(order);
         }
 
         // POST: Order/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        [HttpPost("admin/order/edit/{id}")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(string id, [Bind("OrderId,UserId,TotalPrice,CreateDate")] Order order)
         {
@@ -123,6 +127,7 @@ namespace MAQFurni.Areas_Order_Controllers
         }
 
         // GET: Order/Delete/5
+        [HttpGet("admin/order/delete/{id}")]
         public async Task<IActionResult> Delete(string id)
         {
             if (id == null)
@@ -142,7 +147,7 @@ namespace MAQFurni.Areas_Order_Controllers
         }
 
         // POST: Order/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost("admin/order/delete/{id}"), ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(string id)
         {
